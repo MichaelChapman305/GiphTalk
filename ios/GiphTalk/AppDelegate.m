fe7b93a7007fe1d0e48dce3057ebbd5359422240
Michael Chapman <chapmanm305@gmail.com>
2018-05-20 20:33:09 -0400

    Add Firebase and Firebase/auth to app

--- ios/GiphTalk/AppDelegate.m
+++ ios/GiphTalk/AppDelegate.m
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+@import Firebase;
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
@@ -14,7 +14,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
- 
+  // Use Firebase library to configure APIs
+  [FIRApp configure];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
